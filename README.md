9fbb12054e7c39967b8bb94a3fcb5b47c36db763
rounak-meyur <45717264+rounak-meyur@users.noreply.github.com>
2020-04-07 22:39:18 -0400

    updated git

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# ECE-6304-project
-This project is the end term project on modeling cyber-physical system vulnerabilities for a power system.
+# Bayesian Attack Tree Based Cyber-Physical Security Assessment
+## Motivation
+The advantage of adding modern day information and communication technology (ICT) in the supervisory control and data acquisition (SCADA) system associated with the power network comes at the cost of an increased risk due to cyber
+intrusion. A well planned malicious attack on the SCADA system can not only compromise the communication network, but also cause catastrophic effects on the power grid in form of a widespread blackout. In this project, a Bayesian attack tree based approach is used to model cyber attacks in the SCADA network and the associated risk is evaluated as the combined effect on the communication and power system. This avoids the detailed modeling of every component in the CPS and considers only the critical vulnerabilities required to be exploited to perform the attack. Furthermore, the model takes into account the skill level of the adversary and the difficulty in intruding through each type of vulnerability. The proposed cyber attack model is applied on the IEEE-39 bus system with an associated SCADA network. The risk of a cyber attack on the critical vulnerabilities is evaluated for the power system.
+
+## Simple Bayesian Attack Tree Model
+![png](figs/fig-simpleattacktree.png)
+
+## Bayesian Attack Tree Models of LAN/SCADA Networks
+Substation LAN Model A | Substation LAN Model B 
+:---: | :---: 
+![png](figs/A-model.png) | ![png](figs/B-model.png)
+Substation LAN Model C | Control Center SCADA Model 
+:---: | :---: 
+![png](figs/C-model.png) | ![png](figs/CC-model.png)
+
+## Mean Time to Compromise
+Substation LAN Model A | Substation LAN Model B 
+:---: | :---: 
+![png](figs/fig-resultA.png) | ![png](figs/fig-resultB.png)
+Substation LAN Model C | Control Center SCADA Model 
+:---: | :---: 
+![png](figs/fig-resultC.png) | ![png](figs/fig-resultCC.png)
+
+## Security Comparison
+Substation LAN Models | Substation LAN Models and SCADA 
+:---: | :---: 
+![png](figs/fig-compare-subs.png) | ![png](figs/fig-compare-model.png)
+
+## Physical Impact of an Attack
+IEEE 39 bus system | Impact of attack on different IED sets 
+:---: | :---: 
+![png](figs/fig-ieee39.png) | ![png](figs/fig-sub2.png)
+
+## Risk Assessment
+Attack on Substation LAN Models | Attack on Control Center SCADA 
+:---: | :---: 
+![png](figs/fig-impact1.png) | ![png](figs/fig-impact2.png)
--- src/impact-analysis-ver0.1.py
+++ src/impact-analysis-ver0.1.py
@@ -8,7 +8,7 @@ Create 2D bar graphs
 import os, sys
 import cPickle as pkl
 import matplotlib.pyplot as plt
-import numpy as np
+from matplotlib.lines import Line2D
 import itertools
 
 
@@ -21,24 +21,37 @@ pathFig = workPath + "\\Figures\\"
 sys.path.append(pathLib)
 
 
-Impact = pkl.load(open(pathBin+"Impact_pkl_file.p",'rb'))
+Impact = pkl.load(open(pathBin+"Impact_pkl_file.p",'r'))
 Sub2IED = pkl.load(open(pathBin+"sub2ied_pkl_file.p",'rb'))
 
 
-for sub in range(1,28):
-    dataset = Impact[sub]
-    
-    IED_list = Sub2IED[sub].keys()
-    IED_attacklist = []
-    for r in range(1,len(IED_list)+1):
-        IED_attacklist.extend(list(itertools.combinations(IED_list, r)))
-    
-    impactlist = [Impact[sub][''.join([str(x) for x in alist])] for alist in IED_attacklist]
-    
-    figfile = "Substation"+str(sub)+".png"
-    f = plt.figure(figsize=(10,6))
-    y_pos = np.arange(1,len(impactlist)+1)
-    plt.bar(y_pos, impactlist, align='center', color='b')
-    plt.ylabel('Impact of IED attack',fontsize=15)
-    plt.title('Impact of attacking IEDs of substation'+str(sub),fontsize=15)
-    f.savefig(pathFig+figfile, bbox_inches='tight')       
+sub = 2
+dataset = Impact[sub]
+
+IED_list = Sub2IED[sub].keys()
+IED_attacklist = []
+for r in range(1,len(IED_list)+1):
+    IED_attacklist.extend(list(itertools.combinations(IED_list, r)))
+
+figfile = "Substation"+str(sub)+".png"
+f = plt.figure(figsize=(15,9))
+ax = f.add_subplot(1,1,1)
+color_code = {1:'royalblue',2:'darkorchid',3:'seagreen',4:'orange',5:'lightcoral',
+              6:'crimson'}
+for i,alist in enumerate(IED_attacklist):
+    key = ''.join([str(x) for x in alist])
+    ax.bar(i+1,Impact[sub][key],align='center',color=color_code[len(alist)])
+
+ax.set_ylabel('Impact of IED attack',fontsize=20)
+ax.set_xlabel('IED set attacked',fontsize=20)
+ax.set_title('Impact of attacking IEDs of substation'+str(sub),fontsize=20)
+ax.tick_params(bottom=False,labelbottom=False,left=False,labelleft=False)
+leglines = [Line2D([0], [0], color='royalblue', markerfacecolor='black', marker='o',markersize=0),
+            Line2D([0], [0], color='darkorchid', markerfacecolor='crimson', marker='o',markersize=0),
+            Line2D([0], [0], color='seagreen', markerfacecolor='dodgerblue', marker='o',markersize=0),
+            Line2D([0], [0], color='orange', markerfacecolor='green', marker='o',markersize=0),
+            Line2D([0], [0], color='lightcoral', markerfacecolor='red', marker='o',markersize=0),
+            Line2D([0], [0], color='crimson', markerfacecolor='dodgerblue', marker='o',markersize=0)]
+ax.legend(leglines,['1 IED','2 IEDs','3 IEDs','4 IEDs','5 IEDs','6 IEDs'],
+                  loc='best',ncol=2,prop={'size': 20})
+f.savefig(pathFig+figfile, bbox_inches='tight')
